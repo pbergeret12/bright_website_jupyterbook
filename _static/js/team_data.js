a5b611c5599ed1c8296b3ba7e77ffcd7266c4ad6
claraElk <clara.elkhantour@gmail.com>
2025-09-30 15:25:41 -0400

    Update pages
    * Add L.W and A.H to the team page
    * Add content to the research topic pages
    * Add logos

---
+++ _static/js/team_data.js
@@ -0,0 +1,94 @@
+// _static/team_data.js
+window.TEAM_DATA = [
+  {
+    section: "Principal Investigator",
+    members: [
+      {
+        name: "Clara A. Moreau",
+        title: "M.Sc., Ph.D (Principal Investigator)",
+        image: "cmoreau.jpeg",
+        links: {
+          LinkedIn: "https://www.linkedin.com/in/clara-a-moreau-b44b6858/",
+          Scholar: "https://scholar.google.com/citations?user=tkN9j_cAAAAJ"
+        },
+        bio: `Clara Moreau has a transdisciplinary background that integrates neuroscience, genetics, psychology, and bioinformatics. This unique combination of disciplines has enabled her to tackle fundamental psychiatric problems from innovative perspectives. Nonetheless, her primary expertise lies in neuroimaging. Clara Moreau has more than 10 years of experience in the use of structural, functional, and diffusion MRI, acquired in renowned imaging laboratories such as MNI (McGill, Montreal), the Imaging Genetics Center (USC, Los Angeles), and Neurospin (Saclay).`
+      }
+    ]
+  },
+  {
+    section: "PhD Students",
+    members: [
+      {
+        name: "Pierre Bergeret",
+        title: "M.Sc, Ph.D. Student",
+        program: "Psychiatric Sciences and Addictology",
+        university: "University of Montréal",
+        image: "pbergeret.jpeg",
+        links: {
+          LinkedIn: "https://www.linkedin.com/in/pierrebergeret",
+          GitHub: "https://github.com/pbergeret12"
+        },
+        bio: `Pierre Bergeret holds a Bachelor’s degree in Biology and two Master’s degrees, one in Behavioral Neuroscience and the other in Bioinformatics, completed during his studies in France. Following his final-year internship, he specialized in the analysis of neuroimaging data. With this interdisciplinary background, he is currently pursuing a PhD in Psychiatric Sciences and Addictology at the BRIGHT laboratory, focusing on understanding the shared endophenotypes and genetic foundations of various psychiatric conditions.`
+      },
+      {
+        name: "Clara El Khantour",
+        title: "M.Sc, Ph.D. Student",
+        program: "Psychiatric Sciences and Addictology",
+        university: "University of Montréal",
+        image: "celkhantour.jpeg",
+        links: {
+          LinkedIn: "https://www.linkedin.com/in/clara-el-khantour-32706321a/",
+          GitHub: "https://github.com/claraElk",
+          Scholar: "https://scholar.google.com/citations?hl=en&user=XgfuqwMAAAAJ"
+        },
+        bio: `Clara El Khantour holds a bachelor's degree in cognitive neuroscience, followed by a master's in neuroscience, during which she developed an interest in computational neuroscience and neuroimaging analyses. She is currently pursuing a PhD in psychiatry at the Université de Montréal, focusing on the use of neuroimaging to predict the prognosis in eating disorders.`
+      },
+      {
+        name: "Annabelle Harvey",
+        title: "M.Sc, Research Assistant",
+        image: "aharvey.jpg",
+        links: {
+          LinkedIn: "https://www.linkedin.com/in/annabelle-harvey-525932201/",
+          GitHub: "https://github.com/harveyaa"
+        },
+        bio: ``
+      },
+            {
+        name: "Lea Waller",
+        title: "M.Sc, Visiting Ph.D. Student",
+        program: "Computational Neuroscience",
+        university: "Charité - Universitätsmedizin Berlin",
+        image: "lwaller.jpg",
+        links: {
+          LinkedIn: "https://www.linkedin.com/in/lea-waller-90b601250/",
+          GitHub: "https://github.com/HippocampusGirl",
+          Scholar: "https://scholar.google.com/citations?user=GpR_4UsAAAAJ&hl=en"
+        },
+        bio: `Lea Waller is a PhD student with a focus on computational methods for neuroimaging and genetics. Her background is psychology, neuroscience, and computer science. Her doctoral thesis is a genome-wide study of functional brain imaging as part of the ENIGMA consortium. This involves working with a diverse team of collaborators to coordinate the project, and the development of computational methods to deal with combining diverse datasets into a common analysis framework. One important result of my project is the software HALFpipe, of which she is the lead developer. HALFpipe performs automated data analysis for functional brain imaging data and is widely used within the ENIGMA consortium.`
+      }
+    ]
+  }
+];
+
+// Alumni (ex-"Former Interns")
+window.ALUMNI = [
+  {
+    name: "Seann Wang",
+    title: "Intern",
+    image: "seann_wang.jpeg",
+    dates: "February 2025 - June 2025",
+    links: {
+      LinkedIn: "https://www.linkedin.com/in/seann-wang-a72305179/",
+      GitHub: "https://github.com/wangseann"
+    }
+  },
+  {
+    name: "Clara Maria Bridi",
+    title: "Intern",
+    image: "clara_maria.jpeg",
+    dates: "May 2025 - July 2025",
+    links: {
+      LinkedIn: "https://www.linkedin.com/in/clara-maria-bridi-ab7674271/"
+    }
+  }
+];
--- research_anorexia.md
+++ research_anorexia.md
@@ -2,8 +2,17 @@
 
 ![Anorexia](_static/images/anorexia.webp)
 
+<i>Source</i>: Moreau et al., 2025. <i>Nature Mental Health</i>. https://doi.org/10.1038/s44220-025-00447-x 
+
 Anorexia nervosa is associated with one of the highest mortality rates among all psychiatric illnesses. This elevated risk stems from a combination of medical complications due to prolonged malnutrition and suicide, with mortality increasing by approximately 5% for each decade of illness. Typically emerging during adolescence, anorexia is characterized by severe and persistent dietary restriction, low body weight, and distorted body image. Relapse is common after weight restoration, with only ~60% of patients achieving sustained remission. Despite the severity of this condition, the neurobiological mechanisms underlying relapse and treatment response remain poorly understood.  
 
 To address this gap, we are conducting a longitudinal study at CHU Sainte-Justine, following a cohort of patients aged 8 to 16 years at various stages of clinical care and post-hospitalization. At each time point, we collect multimodal brain imaging, along with clinical and cognitive assessments.  
 
 Our goal is to develop predictive tools for relapse risk by examining brain recovery status after weight restoration, combined with clinical markers. Identifying early indicators of relapse could enable more personalized interventions and help prevent the transition to chronic illness. This project is part of a larger international effort that our lab co-leads through the ENIGMA Eating Disorders Working Group, which brings together over 20 research sites worldwide. Beyond its clinical implications, this study provides a unique opportunity to investigate brain plasticity and adaptation mechanisms in the context of prolonged malnutrition.
+
+## Ongoing project: Protocol & Documentation for recruitment
+We are currently buidling documentations to promote our study and facilitate recruitment. The documentation is available in both English and French and is accessible for children, adolescents, and their parents.
+
+<i>Credit: Clara M. Bridi</i>
+
+![BD](_static/images/ED_bd_recruitment.png)
